9914ab8c3d149b674c4f38bf08ae83e0f22db1e5
Kyle Barbary <kylebarbary@gmail.com>
2016-09-14 17:50:45 -0700

    another benchmark plot

--- benchmark.py
+++ benchmark.py
@@ -9,40 +9,84 @@ import matplotlib.pyplot as plt
 
 from bsplines import Spline1D
 
+plt.style.use('bmh')
+
 os.makedirs("benchmarks", exist_ok=True)
 
+def plot_results(results, title, unit, fname):
+    fig, ax = plt.subplots()
+
+    for key, result in results.items():
+        throughputs = result['sizes'] / (1e6 * result['times'])
+        ax.semilogx(result['sizes'], throughputs, ls='-', label=key)
+
+    ax.set_xlabel(unit)
+    ax.set_ylabel("throughput ({} / us)".format(unit))
+    ax.set_title(title)
+    ax.legend(loc='upper left')
+    fig.savefig(os.path.join("benchmarks", fname))
+    plt.close(fig)
+
+
+# --------------------------------------------------------------------------
 # spline creation
+# --------------------------------------------------------------------------
 
-sizes = np.array([10, 100, 1000, 10000, 100000])
-t = np.empty_like(sizes, dtype=np.float64)
-t_scipy = np.empty_like(sizes, dtype=np.float64)
-for i, n in enumerate(sizes):
-    x = np.linspace(0., float(n), n)
-    y = np.sin(x)
+def benchmark_creation_1d(cls, kwargs):
+    sizes = np.array([10, 30, 100, 1000, 10000, 100000])
+    nloops = np.array([max(100000 // sz, 1) for sz in sizes])
+    times = np.empty_like(sizes, dtype=np.float64)
+    for i, n in enumerate(sizes):
+        x = np.linspace(0., float(n), n)
+        y = np.sin(x)
 
-    nloops = max(10000 // n, 1)
+        t0 = time()
+        for _ in range(nloops[i]):
+            cls(x, y, **kwargs)
+        times[i] = (time() - t0) / nloops[i]
 
-    t0 = time()
-    for _ in range(nloops):
-        Spline1D(x, y)
-    t[i] = (time() - t0) / nloops
+    return {'sizes': sizes,
+            'nloops': nloops,
+            'times': times}
 
-    t0 = time()
-    for _ in range(nloops):
-        InterpolatedUnivariateSpline(x, y, ext=3, k=3)
-    t_scipy[i] = (time() - t0) / nloops
+results = {
+    'bsplines.Spline1D': benchmark_creation_1d(Spline1D, {}),
+    'SciPy UnivariateSpline':
+    benchmark_creation_1d(InterpolatedUnivariateSpline, {'ext': 3, 'k': 3})
+}
+plot_results(results, "1-d spline creation", "knots", "1d_create.png")
 
-fig, ax = plt.subplots()
-ax.semilogx(sizes, t / sizes, ls='-', label='Spline1D')
-ax.semilogx(sizes, t_scipy / sizes, ls='-', label='UnivariateSpline')
-ax.set_xlabel("knots")
-ax.set_ylabel("time (s) / knot")
-ax.set_title("Spline creation")
-ax.legend()
-fig.savefig(os.path.join("benchmarks", "1d_create.png"))
-plt.close(fig)
 
-    
+# ----------------------------------------------------------------------------
+# spline evaluation
+# ----------------------------------------------------------------------------
+
+def benchmark_eval_1d(cls, kwargs):
+    sizes = np.array([10, 30, 100, 1000, 10000, 100000])
+    nloops = np.array([max(100000 // sz, 1) for sz in sizes])
+    times = np.empty_like(sizes, dtype=np.float64)
+    for i, n in enumerate(sizes):
+        x = np.linspace(0., 1000., 1000)
+        y = np.sin(x)
+        s = cls(x, y, **kwargs)
+
+        xtest = np.linspace(0., 1000., n)
+        t0 = time()
+        for _ in range(nloops[i]):
+            s(xtest)
+        times[i] = (time() - t0) / nloops[i]
+
+    return {'sizes': sizes,
+            'nloops': nloops,
+            'times': times}
+
+results = {
+    'bsplines.Spline1D': benchmark_eval_1d(Spline1D, {}),
+    'SciPy UnivariateSpline':
+    benchmark_eval_1d(InterpolatedUnivariateSpline, {'ext': 3, 'k': 3})
+}
+plot_results(results, "1-d spline evaluation", "points", "1d_eval.png")
+
 x = np.linspace(0., 1000., 1000)
 xtest = np.linspace(0., 1000., 100000)
 y = np.sin(x)
